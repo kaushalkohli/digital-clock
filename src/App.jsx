df519bb744468048db26afcc51435a6e64931914
kaushal kohli <kaushalkohli08@gmail.com>
2025-08-08 21:37:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AnalogClock from './components/AnalogClock'
 
 export default function App() {
   return (
-    <div className='container min-h-screen '>
+    <div className='container min-h-screen flex flex-col items-center '>
       <AnalogClock/>
       <Clock/>
       <Weather/>
